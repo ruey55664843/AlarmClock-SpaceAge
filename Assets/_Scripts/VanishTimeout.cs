99a6f1c1c7fb0887659db2f446631f09e68a6995
ruey55664843 <rayyoyo2003@gmail.com>
2017-06-17 10:21:32 +0800

    merge

---
+++ Assets/_Scripts/VanishTimeout.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishTimeout : MonoBehaviour {
+
+
+	public GameObject Minion;
+	public int TimeOut;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SelfDestruct());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(Camera.main.transform);
+	}
+
+	IEnumerator SelfDestruct (){
+		yield return new WaitForSeconds(TimeOut);
+		Destroy (Minion);
+	}
+}
