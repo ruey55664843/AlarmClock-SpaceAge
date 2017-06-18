e0392b87d8bad12872ccaacc90c316912984110f
ruey55664843 <rayyoyo2003@gmail.com>
2017-06-19 02:47:29 +0800

    add HPBar and CooldownBar

---
+++ Assets/_Scripts/CooldownBarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CooldownBarController : MonoBehaviour {
+
+	public GameObject s1;
+	public GameObject s2;
+	public GameObject s3;
+	public GameObject s4;
+	public GameObject s5;
+
+	public float cooldown;
+
+	void Start () {
+		UpdateCooldownStatus (100f);
+	}
+
+	void Update () {
+		UpdateCooldownStatus (cooldown);
+	}
+
+	void UpdateCooldownStatus (float cooldown){
+		s5.SetActive ((cooldown >= 100f) ? true : false);
+		s4.SetActive ((cooldown >= 80f) ? true : false);
+		s3.SetActive ((cooldown >= 60f) ? true : false);
+		s2.SetActive ((cooldown >= 40f) ? true : false);
+		s1.SetActive ((cooldown >= 20f) ? true : false);
+	}
+}
---
+++ Assets/_Scripts/HealthBarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour {
+
+	public GameObject r1;
+	public GameObject o1;
+	public GameObject o2;
+	public GameObject o3;
+	public GameObject y1;
+	public GameObject y2;
+	public GameObject y3;
+	public GameObject g1;
+	public GameObject g2;
+	public GameObject g3;
+
+	public float health;
+
+	void Start () {
+		UpdateHealthStatus (100f);
+	}
+
+	void Update () {
+		UpdateHealthStatus (health);
+	}
+
+	void UpdateHealthStatus (float health){
+		g3.SetActive ((health >= 90f) ? true : false);
+		g2.SetActive ((health >= 80f) ? true : false);
+		g1.SetActive ((health >= 70f) ? true : false);
+		y3.SetActive ((health >= 60f) ? true : false);
+		y2.SetActive ((health >= 50f) ? true : false);
+		y1.SetActive ((health >= 40f) ? true : false);
+		o3.SetActive ((health >= 30f) ? true : false);
+		o2.SetActive ((health >= 20f) ? true : false);
+		o1.SetActive ((health >= 10f) ? true : false);
+		r1.SetActive ((health >= 0f) ? true : false);
+	}
+}
